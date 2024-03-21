a5e3d17e18573692d2affc46448858fe23927c41
Christopher Cantu <chrischimi02@gmail.com>
2024-03-20 23:53:27 -0500

    complete

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal");
+  modal.className = "hidden";
+  const modalMessage = document.getElementById("modal-message");
+  modalMessage.classList.add("hidden");
 
-
-
+  const hearts = document.querySelectorAll(".like-glyph");
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", function (e) {
+      mimicServerCall()
+        .then(() => {
+          if (e.target.innerText === EMPTY_HEART) {
+            e.target.innerText = FULL_HEART;
+            e.target.className = "activated-heart";
+          } else {
+            e.target.innerText = EMPTY_HEART;
+            e.target.className = "";
+          }
+        })
+        .catch((error) => {
+          modal.className = "";
+          modal.innerText = error;
+          setTimeout(() => {
+            modal.className = "hidden";
+          }, 5000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
